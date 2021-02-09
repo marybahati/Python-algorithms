7781ba3b4265d5ed3c7e2ba234e833a073cc13fa
marybahati <bahatimary745@gmail.com>
2021-02-09 12:53:43 +0300

    variables and types

---
+++ Variables and Types.py
@@ -0,0 +1,36 @@
+Python 3.8.4 (tags/v3.8.4:dfa645a, Jul 13 2020, 16:30:28) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> myint = 9
+>>> print(myint)
+9
+>>> floatnumber = 3.1
+>>> print(floatnumber)
+3.1
+>>> myfloat = float(2)
+>>> print(myfloat)
+2.0
+>>> mystring = "Don't get out"
+>>> print(mystring)
+Don't get out
+>>> one = 5
+>>> two = 8
+>>> three = one + two
+>>> print(three)
+13
+>>> a,b = 9,2
+>>> print(a,b)
+9 2
+>>>  # Exercise on variables and types
+ 
+>>> mystring = "hello"
+
+>>> myfloat = 10.0
+>>> myint = 20
+>>> addition = myfloat + myint
+>>> print("mystring")
+mystring
+>>> print(mystring)
+hello
+>>> print(addition)
+30.0
+>>> 
---
+++ print statement.py
@@ -0,0 +1,13 @@
+Python 3.8.4 (tags/v3.8.4:dfa645a, Jul 13 2020, 16:30:28) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('my first line')
+my first line
+>>> print("Hello World.")
+Hello World.
+>>> x = 2
+>>> if x == 2 :
+	print('My variable x is equal to 2')
+
+	
+My variable x is equal to 2
+>>> 
